131757138c4ce9ad0c702a3919ff92a990603804
Mahmoud Mansour <mahmodlte@gmail.com>
2020-09-11 16:35:00 +0300

    Updates

---
+++ precache-manifest.dd233ecbcf62717c20704646c8d9d599.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a44233ea09a07fa4464fc54cdbc1706",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e8e91c88e8a4e0e59fd5",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "19d8a4188aae4502ed3d",
+    "url": "/portfolio/static/css/main.437aa641.chunk.css"
+  },
+  {
+    "revision": "e8e91c88e8a4e0e59fd5",
+    "url": "/portfolio/static/js/2.0f9f1913.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.0f9f1913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d8a4188aae4502ed3d",
+    "url": "/portfolio/static/js/main.369d36d8.chunk.js"
+  },
+  {
+    "revision": "fb0d7fd138b800873680",
+    "url": "/portfolio/static/js/runtime-main.19c3cb22.js"
+  },
+  {
+    "revision": "dd3819b00b6035a3f8f2ca0450f30a2b",
+    "url": "/portfolio/static/media/Train.dd3819b0.png"
+  },
+  {
+    "revision": "46ec7a44273fa302cf6a405a4e64d189",
+    "url": "/portfolio/static/media/avatar.46ec7a44.jpg"
+  }
+]);
